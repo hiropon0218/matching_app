73ed49524e9ac71e93d6559e264f0503cd7d69c7
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2021-01-26 23:30:03 +0900

    ストロングパラメーターの追加
    nameとself_introductionの編集を可能にした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :self_introduction])
   end
 end
